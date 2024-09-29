da90269b34e372976658ef38d344b9ebc1c94761
Rashindra Yadav <yadavrashindra5@gmail.com>
2024-09-29 18:20:32 +0530

    create function

--- index.js
+++ index.js
@@ -26,8 +26,12 @@ const sub = () => {
 };
 
 sub();
-divide(9,3);
+divide(9, 3);
 
-const add1=(a,b,c)=>{
-  return a+b+c;
+const add1 = (a, b, c) => {
+  return a + b + c;
+};
+
+function xyz() {
+  console.log("Hello");
 }
