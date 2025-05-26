dd1c27d3971322133c82788cc6be38d8e8250f23
Rashindra Yadav <yadavrashindra5@gmail.com>
2025-05-26 16:13:48 +0530

    commit -7

--- index.js
+++ index.js
@@ -35,12 +35,5 @@ const add1 = (a, b, c) => {
 function xyz() {
   console.log("Hello");
 }
-console.log("commit1");
-
-console.log("commit 2");
-console.log("commit 3");
-
-console.log("commit 4");
-
-console.log("commit 5");
-console.log("commit 6");
+console.log("commit 7");
+console.log("commit 8");
