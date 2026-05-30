8a4f5857538b31f805db08760e7963e40a72aeba
Rashindra Yadav <rashindrayadav@Rashindras-MacBook-Air.local>
2026-05-30 10:50:02 +0530

    feature 1 commit 5

--- index.js
+++ index.js
@@ -12,13 +12,4 @@ console.log("commit 2")
 console.log("feature 1 commit 3")
 
 console.log("feature 1 commit 4")
-
-console.log("feature 2 commit 3")
-
-console.log("feature 2 commit 4")
-
-console.log("feature 2 commit 5")
-
-console.log("feature 2 commit 6")
-
-console.log("feature 2 commit 7");
+console.log("feature 1 commit 5")
